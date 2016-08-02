8ab3ab71a9eb52319a7b886964d932a841643caf
Marcela Hrda <marcelka.hrda@gmail.com>
2016-08-02 16:56:29 +0800

    Stop using .jsx file extension; fix gulpfile and package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,29 +3,25 @@
 let gulp = require('gulp')
 let browserify = require('browserify')
 let source = require('vinyl-source-stream')
-let babelify = require('babelify')
 let babel = require('gulp-babel')
 let eslint = require('gulp-eslint')
 
 let BUILD_FOLDER = './public/'
 let argv = require('yargs').alias('e', 'example').default('e', 'example1').argv
 
-// Build JS and JSX and copy it to build folder
+// Build JS and copy it to build folder
 gulp.task('js', function() {
-  return browserify(`./${argv.example}/main-client.jsx`, {
-    extensions: ['.jsx']
-  }).transform(babelify.configure({
-    stage: 0
-  })).bundle()
+  return browserify(`./${argv.example}/main-client.js`, {
+    extensions: ['.js']
+  }).transform('babelify', {presets: ['es2015', 'react', 'stage-0']})
+    .bundle()
     .pipe(source('main.js'))
     .pipe(gulp.dest(BUILD_FOLDER + 'js/'))
 })
 
 gulp.task('build-dist', function() {
-  return gulp.src('lib/**/*.jsx')
-    .pipe(babel({
-      stage: 0
-    }))
+  return gulp.src('lib/**/*.js')
+    .pipe(babel({stage: 0}))
     .pipe(gulp.dest('dist'))
 })
 
@@ -46,9 +42,7 @@ gulp.task('eslint', () => {
   return gulp.src([
     'gulpfile.babel.js',
     'lib/**/*.js',
-    'lib/**/*.jsx',
     'example1/**/*.js',
-    'example1/**/*.jsx',
   ])
   .pipe(eslint())
   .pipe(eslint.format())
