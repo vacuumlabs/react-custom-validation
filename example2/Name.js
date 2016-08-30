3c2381c27745d2e9fdae526eacb8a728c1219d99
Marcela Hrda <marcelka.hrda@gmail.com>
2016-08-31 00:03:29 +0800

    Fix small bug in example2

--- example2/Name.js
+++ example2/Name.js
@@ -79,7 +79,7 @@ class Name extends React.Component {
             fieldEvent('change', name)
           }}
           onBlur={(e) => fieldEvent('blur', name)}
-          value={this.props.state.fields[name].value}
+          value={this.props.state.fields[name]}
         />
         <FormControl.Feedback />
       </FormGroup>
