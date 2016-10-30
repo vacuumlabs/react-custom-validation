19173541374d937c14b9ca5bd46cf493f38894ad
Marcela Hrda <marcelka.hrda@gmail.com>
2016-10-30 14:07:50 +0100

    Polish example1, add example2

--- pages/example/1.js
+++ pages/example/1.js
@@ -10,11 +10,6 @@ class App extends React.Component {
       password: '',
       rePassword: ''
     },
-    validation: {
-      email: {},
-      password: {},
-      rePassword: {}
-    }
   }
 
   fieldChange = (field, value) => {
@@ -22,19 +17,14 @@ class App extends React.Component {
     this.setState(this.state)
   }
 
-  onValidation = (name, data) => {
-    this.state.validation[name] = data
-    this.setState(this.state)
-  }
-
   render() {
-    return <Form
+    return (<Form
       fields={this.state.fields}
       onChange={this.fieldChange}
-      onSubmit={(isValid) => isValid ? alert("Submitting") : alert("Error!")}
+      onValid={() => alert('Submitting...')} // eslint-disable-line no-alert
+      onInvalid={() => alert('Error!')} // eslint-disable-line no-alert
       onValidation={this.onValidation}
-      validation={this.state.validation}
-    />
+    />)
   }
 }
 
@@ -82,27 +72,27 @@ function validationConfig(props) {
 
 class Form extends React.Component {
   render() {
-    const {fields, onChange, onSubmit, connectField, validation} = this.props
+    const {fields, onChange, onValid, onInvalid, connectField, validation} = this.props
     return (
       <form className={style}>
         <label>Email</label>
-        <input type='text' value={fields.email}
+        <input type="text" value={fields.email}
           {...connectField('email', (e) => onChange('email', e.target.value))}/>
         {validation.email.show && <span>{validation.email.error.reason}</span>}
 
         <label>Password</label>
-        <input type='text' value={fields.password}
+        <input type="text" value={fields.password}
           {...connectField('password', (e) => onChange('password', e.target.value))}/>
         {validation.password.show && <span>{validation.password.error.reason}</span>}
 
         <label>Repeat password</label>
-        <input type='text' value={fields.rePassword}
+        <input type="text" value={fields.rePassword}
           {...connectField('rePassword', (e) => onChange('rePassword', e.target.value))}/>
         {validation.rePassword.show && <span>{validation.rePassword.error.reason}</span>}
 
         <button onClick={(e) => {
           e.preventDefault()
-          this.props.onFormValid(onSubmit)
+          this.props.submit(onValid, onInvalid)
           this.props.fieldEvent('submit')
         }}>Sign up</button>
       </form>
--- pages/example/2.js
+++ pages/example/2.js
@@ -1,5 +1,174 @@
 import React from 'react'
+import Promise from 'bluebird'
+import {validated} from '../../lib'
 
-export default () => (
-  <h1>Example 2</h1>
-)
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+
+const OPERATIONS = [
+  {id: 'add', symbol: '+', fn: add, desc: 'Addition'},
+  {id: 'multiply', symbol: '*', fn: multiply, desc: 'Multiplication'},
+  {id: 'subtract', symbol: '-', fn: subtract, desc: 'Subtraction'},
+]
+
+class App extends React.Component {
+  state = {
+    problems: {
+      id1: {number1: '', number2: '', result: ''}
+    },
+    operation: OPERATIONS[0],
+    lastId: 1,
+  }
+
+  numberChange = (id, number, value) => {
+    this.state.problems[id][number] = value
+    this.setState(this.state)
+  }
+
+  operationChange = (value) => {
+    let operation = OPERATIONS.filter((o) => o.id === value)[0]
+    this.setState({operation})
+  }
+
+  addProblem = () => {
+    this.state.lastId += 1
+    this.state.problems[`id${this.state.lastId}`] = {number1: '', number2: '', result: ''}
+    this.setState(this.state)
+  }
+
+  removeProblem = (id) => {
+    delete this.state.problems[id]
+    this.setState(this.state)
+  }
+
+  render() {
+    return (<Math
+      problems={this.state.problems}
+      operation={this.state.operation}
+      operations={OPERATIONS}
+      numberChange={this.numberChange}
+      operationChange={this.operationChange}
+      addProblem={this.addProblem}
+      removeProblem={this.removeProblem}
+      onValid={() => alert('Everything correct!')} // eslint-disable-line no-alert
+      onInvalid={() => alert('Something is wrong.')} // eslint-disable-line no-alert
+      onValidation={this.onValidation}
+    />)
+  }
+}
+
+class Math extends React.Component {
+
+  renderSingle(id) {
+    let {problems, validation, operation, numberChange, removeProblem, connectField} = this.props
+    let {number1, number2, result} = problems[id]
+    let {isValid, error: {reason}, show} = validation[id]
+
+    let validationMessage = {
+      true: 'Correct!',
+      null: 'Computation in progress...',
+      false: reason
+    }[isValid]
+    let style = {
+      true: {color: 'green'},
+      null: {},
+      false: {color: 'red'}
+    }[isValid]
+
+    return (
+      <div key={id}>
+        <input
+          type="text"
+          value={number1}
+          {...connectField(`${id}-number1`, (e) => numberChange(id, 'number1', e.target.value))}
+        />
+        {operation.symbol}
+        <input
+          type="text"
+          value={number2}
+          {...connectField(`${id}-number2`, (e) => numberChange(id, 'number2', e.target.value))}
+        />
+        =
+        <input
+          type="text"
+          value={result}
+          {...connectField(`${id}-result`, (e) => numberChange(id, 'result', e.target.value))}
+        />
+        <button type="button" onClick={(e) => removeProblem(id)}>X</button>
+        {show && <span style={style}>{validationMessage}</span>}
+      </div>
+    )
+  }
+
+  render() {
+    let {problems, operationChange, operation, operations, addProblem} = this.props
+    let problemIds = Object.keys(problems)
+
+    return (
+      <form
+        onSubmit={
+          (e) => {
+            e.preventDefault()
+            let {onValid, onInvalid} = this.props
+            this.props.fieldEvent('submit')
+            this.props.submit(onValid, onInvalid)
+          }
+        }
+      >
+        <div>
+          <label>Select operation</label>
+          <select
+            value={operation.id}
+            onChange={(e) => operationChange(e.target.value)}
+          >
+            {operations.map((o) => <option key={o.id} value={o.id}>{o.desc}</option>)}
+          </select>
+        </div>
+          {problemIds.map((id) => this.renderSingle(id))}
+        <div>
+          <button type="button" onClick={(e) => addProblem()}> Add another </button>
+          <button type="submit"> Check </button>
+        </div>
+      </form>
+    )
+  }
+}
+
+function isNumber(value) {
+  return (value === '' || isNaN(value)) ? 'All fields must be numbers!' : null
+}
+
+function isResultCorrect(number1, number2, result, operation) {
+  let i = (n) => parseInt(n, 10)
+  return Promise.delay(3000).then(() =>
+    operation(i(number1), i(number2)) === i(result) ? null : 'Wrong!'
+  )
+}
+
+function validationConfig(props) {
+  let {problems, operation} = props
+  let validations = {}
+  let fields = []
+
+  for (let id in problems) {
+    let {number1, number2, result} = problems[id]
+    let _fields = [`${id}-number1`, `${id}-number2`, `${id}-result`]
+    validations[id] = {
+      rules: {
+        isNumber1: [isNumber, number1],
+        isNumber2: [isNumber, number2],
+        isNumberResult: [isNumber, result],
+        isResultCorrect: [isResultCorrect, number1, number2, result, operation.fn]
+      },
+      fields: _fields
+    }
+    fields = [...fields, ..._fields]
+  }
+
+  return {fields, validations}
+}
+
+Math = validated(validationConfig)(Math)
+
+export default () => <App />
